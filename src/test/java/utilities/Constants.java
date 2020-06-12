e3e086741b0419dece2e65f869aa87a9085d5fef
Sayan De Roy <sayan.de.roy@gmail.com>
2020-06-12 15:40:50 +0530

    Constants File change

--- src/test/java/utilities/Constants.java
+++ src/test/java/utilities/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static String excelPath = System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\TestDataAndFlow.xlsx";
     public static String chromeExecutablePath = System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe";
     public static String ffExecutablePath = System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\geckodriver.exe";
-    public static String extentReportPath = System.getProperty("user.dir") + "\\reports\\extentReports.html";
+    public static String extentReportPath = System.getProperty("user.dir") + "\\target\\reports\\extentReports.html";
     public static String extentConfigPath = System.getProperty("user.dir") + "\\src\\test\\resources\\extentconfig\\ReportsConfig.xml";
-    public static String screenShotPath = System.getProperty("user.dir") + "\\reports\\screenshots\\";
+    public static String screenShotPath = System.getProperty("user.dir") + "\\target\\reports\\screenshots\\";
 }
